be216202ce8fdbfcf242e6cf7a5a663c267381a7
Anton Hvornum <anton.feeds+github@gmail.com>
2019-10-13 00:05:08 +0200

    Created a more transparent handling of vertices objects. Prepared a general shape class as well that will remove a lot of the headache of doing the pyglet.graphics.draw() later on

--- test.py
+++ test.py
@@ -1,21 +1,34 @@
 from pyglet_gui import *
 from random import randint
 
-class circle(generic_sprite):
+class circle(genericSprite):
 	def __init__(self, *args, **kwargs):
 		super(circle, self).__init__(*args, **kwargs)
-		self.circle = gfx_helpers.create_circle(self.x, self.y, batch=self.batch)
-		
+		## TODO: Add batch
+		self.circle = gfx.create_circle(kwargs['x'], kwargs['y'], batch=self.batch)
+		if self.batch:
+			self.vertices = self.circle['blob'].vertices
+		else:
+			self.vertices = self.circle
+
 	def update(self):
 		## == Iterate through the vertices and move their
 		##    x and y coordinates accordingly.
 		##
 		new_vertices = []
 		x, y = randint(-1, 1), randint(-1, 1)
-		for index in range(0, len(self.circle['blob'].vertices),2):
-			old_x, old_y = self.circle['blob'].vertices[index:index+2]
+		for index in range(0, len(self.vertices),2):
+			old_x, old_y = self.vertices[index:index+2]
 			new_vertices += [old_x+x, old_y+y]
-		self.circle['blob'].vertices = new_vertices
+
+		self.vertices = new_vertices
+
+	def render(self):
+		colors = gfx.hex_to_colorpair(gfx.colors['tea_green'])*int(len(self.vertices)/2)
+		pyglet.graphics.draw(int(len(self.vertices)/2), pyglet.gl.GL_TRIANGLES,
+			('v2i/stream', self.vertices),
+			('c3B', colors)
+		)
 
 class smilyface(genericSprite):
 	def __init__(self, *args, **kwargs):
@@ -45,9 +58,10 @@ class window(windowWrapper):
 
 		## == Create 100 random circles that will float around in space.
 
-		for i in range(100):
-			self.add_sprite('circle'+str(i), circle(x=int(self.width/2), y=int(self.height/2), alpha=0))
+		#for i in range(100):
+		#	self.add_sprite('circle'+str(i), circle(x=int(self.width/2), y=int(self.height/2), alpha=0))
 
+		self.add_sprite('circle', circle(x=int(self.width/2), y=int(self.height/2), alpha=0))
 		self.add_sprite('smilyface', smilyface(x=100, y=100, width=20, height=20, alpha=0))
 
 W = window()
